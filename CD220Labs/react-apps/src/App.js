32df3f753d9f85a1880dbabb5531dc8bd09c24ca
uniqcode87 <54540612+uniqcode87@users.noreply.github.com>
2022-09-07 16:35:08 +0700

    Step 11 Hands-on Lab Module 4

--- CD220Labs/react-apps/src/App.js
+++ CD220Labs/react-apps/src/App.js
@@ -1,9 +1,48 @@
-function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+// function App(props) {
+//   const colorStyle = {
+//     color:props.color,
+//     fontSize:props.size+"px",
+//     backgroundColor:props.backgroundColor,
+//   }
+//   return (
+//     <div style={colorStyle}>
+//       Hello World!
+//     </div>
+//   );
+// }
+
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  state = { counter:0 }
+
+  incrementCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    const colorStyle = {
+      color:this.props.color,
+      fontSize:this.props.size+"px",
+      backgroundColor:this.props.backgroundColor,
+    }
+    return (
+      <div>
+        <div style={colorStyle}>
+          React Component
+          <br/><br/>
+          <button onClick={this.incrementCounter}>Click Me!</button>
+          <br/><br/>
+          {this.state.counter}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- CD220Labs/react-apps/src/index.js
+++ CD220Labs/react-apps/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App color="green" size="20" backgroundColor="red"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
