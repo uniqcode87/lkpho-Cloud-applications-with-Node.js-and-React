d8eb940175f6083fd0a0644c8a23459a74e72691
uniqcode87 <54540612+uniqcode87@users.noreply.github.com>
2022-09-08 15:49:24 +0700

    Last commit for Module 4

--- CD220Labs/react-apps/src/App.js
+++ CD220Labs/react-apps/src/App.js
@@ -17,15 +17,10 @@ class App extends React.Component {
     console.log(req);
     req.then(resp => {
       let listOfEvents = resp.data.authorEvent;
-      console.log(listOfEvents);
-      let listOfEventsAsArray = Object.entries(listOfEvents);
-      console.log(listOfEventsAsArray);
-      let eventDetails = listOfEventsAsArray.map((eventDetial)=>{
-        let eventListCollection = Object.entries(eventDetial[1])
-        console.log(eventListCollection);
-        return <div>{eventListCollection[4][1]} on {eventListCollection[5][1]} at {eventListCollection[7][1]} in {eventListCollection[10][1]} - {eventListCollection[11][1]}</div>
+      let eventList = listOfEvents.map((event)=>{
+        return {author: event.eventauthor, date: event.eventdate, time: event.eventtime, location: event.location, market: event.market};
       });
-      this.setState({eventList:eventDetails});
+      this.setState({eventList:eventList});
     }).catch(err => {
       console.log(err.toString());
     });
@@ -41,13 +36,31 @@ class App extends React.Component {
     return (
       <div style={colorStyle}>
         React Component
-        <ul>
+        <table style={{border: "1px solid black"}}>
+          <thead>
+            <th style={{border: "1px solid black"}}>No</th>
+            <th style={{border: "1px solid black"}}>Author</th>
+            <th style={{border: "1px solid black"}}>Date</th>
+            <th style={{border: "1px solid black"}}>Time</th>
+            <th style={{border: "1px solid black"}}>Location</th>
+            <th style={{border: "1px solid black"}}>Market</th>
+          </thead>
+          <tbody>
+            
+          </tbody>
           {
-            this.state.eventList.map(function(eventDetails) {
-              return <li key={li_ctr++}>{eventDetails}</li>;
+            this.state.eventList.map(function(event) {
+              return <tr>
+                      <td style={{border: "1px solid black"}}>{li_ctr++}</td>
+                      <td style={{border: "1px solid black"}}>{event.author}</td>
+                      <td style={{border: "1px solid black"}}>{event.date}</td>
+                      <td style={{border: "1px solid black"}}>{event.time}</td>
+                      <td style={{border: "1px solid black"}}>{event.location}</td>
+                      <td style={{border: "1px solid black"}}>{event.market}</td>
+                    </tr>;
             })
           }
-        </ul>
+        </table>
       </div>
     );
   }
